8e6ad646629ead64be694d7398918c6491274413
zahidalik <zahidkhan545@icloud.com>
2022-01-30 23:15:42 +0300

    add sessions and users controller, custom css, background images

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  
   root "home#home"
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  post "logout", to: "sessions#destroy"
+
+  resources :users
 end
